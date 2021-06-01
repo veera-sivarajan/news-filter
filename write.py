64fde9e3d99fdff24db60c1d93d322a4e88e97a8
Veera Sivarajan <sveera.2001@gmail.com>
2021-06-01 00:22:12 -0400

    Add code to write all news link on a file
    All news URLs along with their corresponding titles will be
    written to a new html file.

---
+++ write.py
@@ -0,0 +1,25 @@
+from data import *
+import os
+
+os.remove("news.html")
+head = """<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href=/home/veera/Projects/Blog/docs/style.css>
+<meta charset="UTF-8">
+<meta name="viewport" content="initial-scale=1">
+<meta name="HandheldFriendly" content="true">
+"""
+data = link_and_titles(cleanse(get_data()))
+
+file = open("news.html", "a")
+file.write(head)
+
+for ele in data:
+    file.write("<li>")
+    line = "<a href=\"" + ele[0] + "\">" + ele[1] + "</a>"
+    file.write(line)
+    file.write("</li>")
+    file.write("\n")
+
+file.close()
+
